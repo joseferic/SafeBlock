2bec8fba2ba9e42832a6b719d5d002ae011c150f
Josef Eric <joseferic5@gmail.com>
2022-05-02 18:56:58 +0700

    list history user, not yet filltered

--- app/src/main/java/com/example/safeblock/HistoryUserFragment.java
+++ app/src/main/java/com/example/safeblock/HistoryUserFragment.java
@@ -9,6 +9,9 @@ import android.os.Bundle;
 
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -22,38 +25,86 @@ import com.google.gson.Gson;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.gas.ContractGasProvider;
+import org.web3j.tx.gas.DefaultGasProvider;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class HistoryUserFragment extends Fragment {
 
     FragmentHistoryUserBinding binding;
 
+
+    public List<Data> listData;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = FragmentHistoryUserBinding.inflate(inflater,container,false);
+        View rootView = inflater.inflate(R.layout.fragment_history_user, container, false);
+        // 1. get a reference to recyclerView
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.history_rv);
+
+        // 2. set layoutManger
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        // this is data from recycler view
+        getAllData();
 
 
-        user_data obj = getUserData();
-
-        if (obj !=null){
-            binding.historyuserTv.setText(obj.toString());
-        }
-        else{
-            binding.historyuserTv.setText("Data User Null");
-        }
-
-        PlaceData obj_place = getPlace();
-
-        if (obj_place !=null){
-            binding.tvTestDataGmaps.setText(obj_place.toString());
-        }
-        else{
-            binding.tvTestDataGmaps.setText("Data Gmaps Null");
-        }
-       // binding.historyuserTv.setText(obj.toString());
-        return binding.getRoot();
+        // 3. create an adapter
+        RecyclerViewAdapter mAdapter = new RecyclerViewAdapter(getContext(),listData);
+        // 4. set adapter
+        recyclerView.setAdapter(mAdapter);
+        return rootView;
     }
 
+    private void getAllData(){
+        listData = new ArrayList<>();
+        final Web3j web3j = Web3j.build(
+                new HttpService(
+                        "https://rinkeby.infura.io/v3/d9100bd917c6448695785e26e5f0a095"
+                )
+        );
+
+        String contractAddress = "0x2efd3Dc020D75f5Abf12C74F011aBB3Be8fc7f6C";
+        String privateKey = "fd20f2be43dd3fa879826279c6067a18aa5b9a40d5ed7f6c2e672e4154876ba5";
+
+        Credentials credentials = Credentials.create(privateKey);
+        ContractGasProvider contractGasProvider = new DefaultGasProvider();
+        UserData_sol_UserData userData = UserData_sol_UserData.load(contractAddress,web3j,credentials,contractGasProvider);
+        try {
+            //userData.create_user_data("Josef Eric","Test Place",formatter.format(date)).sendAsync().get();
+            Log.v("Data length list",userData.get_user_list_length().sendAsync().get().toString());
+            Integer list_length = Integer.valueOf(userData.get_user_list_length().sendAsync().get().toString());
+
+            for (int i=0; i<list_length;i++){
+                Data nData = new Data(
+                        userData.users(BigInteger.valueOf(i)).sendAsync().get().component1(),
+                        userData.users(BigInteger.valueOf(i)).sendAsync().get().component2(),
+                        userData.users(BigInteger.valueOf(i)).sendAsync().get().component3(),
+                        userData.users(BigInteger.valueOf(i)).sendAsync().get().component4(),
+                        userData.users(BigInteger.valueOf(i)).sendAsync().get().component5(),
+                        userData.users(BigInteger.valueOf(i)).sendAsync().get().component6(),
+                        userData.users(BigInteger.valueOf(i)).sendAsync().get().component7()
+                );
+                Log.v("Data User On List ", nData.transaction_id.toString());
+                Log.v("Data User Detail ",userData.users(BigInteger.valueOf(i)).sendAsync().get().toString());
+                listData.add(nData);
+            }
+
+            Log.v("Data List ", String.valueOf(listData));
+            Log.v("Data List ", String.valueOf(listData.size()));
+        }catch (Throwable throwable){
+            throwable.printStackTrace();
+        }
+    }
 
 
     public user_data getUserData(){
@@ -97,4 +148,23 @@ public class HistoryUserFragment extends Fragment {
             }
         }
     }
+
+    //        user_data obj = getUserData();
+//
+//        if (obj !=null){
+//            binding.historyuserTv.setText(obj.toString());
+//        }
+//        else{
+//            binding.historyuserTv.setText("Data User Null");
+//        }
+//
+//        PlaceData obj_place = getPlace();
+//
+//        if (obj_place !=null){
+//            binding.tvTestDataGmaps.setText(obj_place.toString());
+//        }
+//        else{
+//            binding.tvTestDataGmaps.setText("Data Gmaps Null");
+//        }
+    // binding.historyuserTv.setText(obj.toString());
 }
---
+++ app/src/main/java/com/example/safeblock/RecyclerViewAdapter.java
@@ -0,0 +1,61 @@
+package com.example.safeblock;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
+
+    Context context;
+    List<Data> data;
+
+
+    public RecyclerViewAdapter(Context mContenxt, List<Data> mData){
+        this.context = mContenxt;
+        this.data = mData;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v;
+        v = LayoutInflater.from(context).inflate(R.layout.list_item,parent,false);
+        MyViewHolder viewHolder = new MyViewHolder(v);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.tv_placeName.setText(data.get(position).place_visited);
+        holder.tv_date.setText(data.get(position).time_visited);
+        holder.tv_transactionHash.setText(data.get(position)._walletAddress);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView tv_placeName;
+        private TextView tv_date;
+        private TextView tv_transactionHash;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tv_placeName = itemView.findViewById(R.id.tv_title);
+            tv_date = itemView.findViewById(R.id.tv_date);
+            tv_transactionHash = itemView.findViewById(R.id.tv_description);
+
+        }
+    }
+}
